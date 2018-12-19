94fac271da55532c166c448b6496717d483782a8
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-19 22:55:14 +0100

    Add files via upload

---
+++ Equipement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Projet
+{
+    public class Equipement
+    {
+
+            #region Variables
+        public string Name;
+        public string Description;
+        public float Value;
+        public float Weight;
+
+        #endregion
+        public Equipement(string name, string description, float value, float weight)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Value = value;
+            this.Weight = weight;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.Name + " : " + this.Description);
+        }
+
+        // Fonction pour ramasser les Objets
+        virtual public void PickUp()
+        {
+
+        }
+
+        // Fonction pour utiliser les Objets
+        virtual public void Use()
+        {
+
+        }
+
+        /*
+            Virtualisation: Si j'ai besoin d'ajouter des choses specifique a mon Objet
+         */
+    
+
+    }
+}
---
+++ Potion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Projet
+{
+    public class Potion : Equipement
+    {
+
+        public enum PotionType { Heal, Defense, Attack };
+        public PotionType Type;
+
+        public Potion(string name, string description, float value, float weight, PotionType type)
+            : base(name, description, value, weight)
+        {
+            this.Type = type;
+        }
+
+        public override void PickUp()
+        {
+            base.PickUp();
+            Console.WriteLine("Vous avez une nouvelle potion");
+        }
+        public override void Use()
+        {
+            base.Use();
+            Console.WriteLine("Vous avez utilisé une potion");
+        }
+    }
+}
