334d19c1b2a75e41af0a98a1f5556c9e15ad4626
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-20 23:56:06 +0100

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -7,14 +7,14 @@ namespace Projet
             public int Largeur;
             public int Longeur;
             public string Nom;
-            public Case[,] Plateau;
-            public int MaxPlateau;
+            public Case[,] Map;
+            public int MaxMap;
 
             public Map(int largeur, int longueur)
             {
                 Longeur = longueur;
                 Largeur = largeur;
-                Plateau = new Case[largeur, longueur];
+                Map = new Case[largeur, longueur];
                 
 
                 for (int i = 0; i < largeur; i++)
@@ -22,10 +22,10 @@ namespace Projet
                     for (int j = 0; j < longueur; j++)
                     {
                                         
-                            Plateau[i, j] = new Case(i, j, Case.CaseType.Desert, "une case");
+                            Map[i, j] = new Case(i, j, Case.CaseType.Desert, "une case");
                        
 
-                        Plateau[i, j] = new Case();
+                       Map[i, j] = new Case();
 
                     }
                 }
