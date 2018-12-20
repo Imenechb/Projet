afc34654bd4c7a50359160d18eaaedd8e24ad769
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-20 22:31:39 +0100

    Update Equipement.cs

--- Equipement.cs
+++ Equipement.cs
@@ -4,13 +4,12 @@ namespace Projet
     public class Equipement
     {
 
-            #region Variables
         public string Name;
         public string Description;
         public float Value;
         public float Weight;
 
-        #endregion
+        
         public Equipement(string name, string description, float value, float weight)
         {
             this.Name = name;
@@ -24,21 +23,15 @@ namespace Projet
             Console.WriteLine(this.Name + " : " + this.Description);
         }
 
-        // Fonction pour ramasser les Objets
         virtual public void PickUp()
         {
 
         }
 
-        // Fonction pour utiliser les Objets
         virtual public void Use()
         {
 
         }
-
-        /*
-            Virtualisation: Si j'ai besoin d'ajouter des choses specifique a mon Objet
-         */
     
 
     }
