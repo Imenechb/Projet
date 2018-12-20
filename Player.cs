2d47603af5b4ac84cd9d69572a61e191dd848568
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-20 12:37:59 +0100

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -3,8 +3,17 @@ namespace Projet
 {
     public class Player
     {
+        public string NomPlayer;
         public Player()
         {
+            Console.WriteLine("Bienvenu au jeu");
+            Console.WriteLine("Veuillez entrer votre nom s'il vous plaît");
+            string Nom = Console.ReadLine();
+            Nom = NomPlayer;
+
+            Console.Clear();
+            Console.WriteLine("Bienvenu" + Nom);
+            Console.WriteLine("Que l'aventure commence!");
         }
     }
 }
