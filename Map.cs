df11a38fe80baf747cc40446088a49be1716ff8a
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-20 22:23:00 +0100

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -15,18 +15,17 @@ namespace Projet
                 Longeur = longueur;
                 Largeur = largeur;
                 Plateau = new Case[largeur, longueur];
-                // Lire mon tableau qui dans un tableau
+                
 
                 for (int i = 0; i < largeur; i++)
                 {
                     for (int j = 0; j < longueur; j++)
                     {
-                        /*
-                            Si je veux que mon plateau ne soit pas aleatoire                
+                                        
                             Plateau[i, j] = new Case(i, j, Case.CaseType.Desert, "une case");
-                         */
+                       
 
-                        //Plateau[i, j] = new Case();
+                        Plateau[i, j] = new Case();
 
                     }
                 }
