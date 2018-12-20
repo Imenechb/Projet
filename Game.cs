df87b2781c1d4570e6fe0d1b634241591597c207
Imenechb <32359932+Imenechb@users.noreply.github.com>
2018-12-20 22:46:04 +0100

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -3,27 +3,76 @@ namespace Projet
 {
     public class Game
     {
+        public string Name;
+        protected int PointDeVie;
+        protected int Force;
+        protected int Endurance;
+        protected int Taille;
+
+        public string NOMPlayer { get; }
+
+        private string NomPlayer;
+        public static Equipement[] Inventaire;
         public Player player;
         public Map map;
 
+
+        public Game(string Nom)
+        {
+            this.NomPlayer = Nom;
+
+
+            Inventaire = new Equipement[10];
+
+            Inventaire[0] = new Potion("Soin", "Une potion de soin", 1, 2, Potion.PotionType.Heal);
+        }
+
         public Game()
         {
-            //TODO constucteur 
+        }
+
+        public virtual int Damage(int degats)
+        {
+            PointDeVie -= degats;
+            if (PointDeVie <= 0)
+            {
+                Console.WriteLine("DEAD");
+            }
+            return PointDeVie;
+        }
+
+
+        public virtual int Attack()
+        {
+            return 1;
         }
         public void StartGame()
         {
-             Game game = new Game();
+            Game game = new Game();
         }
         public void Deplacement()
         {
-            AfficherMenu();
+            PrintMenu();
             int choice = AskChoice(0, 3);
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Go EAST");
+                    break;
+                case 1:
+                    Console.WriteLine("GO NORTH");
+                    break;
+                case 2:
+                    Console.WriteLine("GO WEST");
+                    break;
+                case 3:
+                    Console.WriteLine("GO SOUTH");
+                    break;
+            }
 
-            // TODO Récupérer la case
-            // TODO tester le contenu de la case
-            // TODO lancer le combat
+           
         }
 
-      
+
     }
 }
